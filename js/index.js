66183c5e6efdd7c7fd6e0d1004a3d75d3cbb0327
mani-monga <mni.monga02@gmail.com>
2022-02-05 09:28:41 +0530

    my resume

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+
+const mynav = document.getElementById('nav');
+const vis = document.getElementById('vishd');
+const visimg = document.getElementById('imge');
+
+
+window.onscroll = function(){
+    "use strict";
+    if(document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280){
+        mynav.style.backgroundColor = '#810000';
+        visimg.style.display='none';
+        vis.style.display='block';
+
+    }
+    else{
+        mynav.style.backgroundColor = '#000';
+        vis.style.display='none';
+        visimg.style.display='block';
+        
+    }
+}
+
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 3000); // Change image every 2 seconds
+} 
