d43373e3f6ca779f0b3f2cf5a5ed7d0e0ac153c5
mani-monga <mni.monga02@gmail.com>
2022-03-19 20:46:45 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
-
-
 const mynav = document.getElementById('nav');
 const vis = document.getElementById('vishd');
 const visimg = document.getElementById('imge');
 
-
 window.onscroll = function(){
     "use strict";
-    if(document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280){
-        mynav.style.backgroundColor = '#810000';
-        visimg.style.display='none';
-        vis.style.display='block';
-        
-      }
-      else{
-        mynav.style.backgroundColor = '#000';
-        visimg.style.animation='rotate 1s linear';
-        vis.style.display='none';
-        visimg.style.display='block';
-        
-    }
+    Addanimation();
+    logoeffect();
+}
+function logoeffect(){
+  if(document.body.scrollTop >= window.screen.height || document.documentElement.scrollTop >= window.screen.height){
+    mynav.style.backgroundColor = '#810000';
+    visimg.style.display='none';
+    vis.style.display='block';
+  }
+  else{
+    mynav.style.backgroundColor = '#000';
+    // visimg.style.animation='rotate 1s linear';
+    vis.style.display='none';
+    visimg.style.display='block';
+  }
 }
 
-var slidePosition = 0;
-SlideShow();
+
+
+   var slidePosition = 0;
+   SlideShow();
 
 function SlideShow() {
   var i;
@@ -34,5 +35,29 @@ function SlideShow() {
   slidePosition++;
   if (slidePosition > slides.length) {slidePosition = 1}
   slides[slidePosition-1].style.display = "block";
-  setTimeout(SlideShow, 3000); // Change image every 2 seconds
+  setTimeout(SlideShow, 2000); // Change image every 2 seconds
 } 
+
+
+
+
+
+function Addanimation(){
+  const a = window.screen.height;
+  if(document.body.scrollTop >= a - 100 || document.documentElement.scrollTop >= a - 100 ){
+    document.getElementById('1').classList.add('fade-left');
+    document.getElementById('2').classList.add('fade-right');
+  
+  }
+  if(document.body.scrollTop >= 2*a|| document.documentElement.scrollTop >= 2*a){
+    document.getElementById('3').classList.add('fade-left');
+    document.getElementById('4').classList.add('fade-right');
+
+  }
+  if(document.body.scrollTop >= 3*a || document.documentElement.scrollTop >= a*3){
+    document.getElementById('5').classList.add('fade-left');
+  
+   }
+
+  }
+
